f5df7c05170789e5effef6c26d589f4dbb82587f
cristcost <cristiano.costantini@gmail.com>
2015-10-10 10:02:03 +0200

    setup of cpp environment

---
+++ cpp/src/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  Sandbox
+//
+//  Created by Cristiano Costantini on 10/10/15.
+//  Copyright (c) 2015 cristcost.net. All rights reserved.
+//
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+
+    cout << "Hello world" << endl;
+
+    return 0;
+}
