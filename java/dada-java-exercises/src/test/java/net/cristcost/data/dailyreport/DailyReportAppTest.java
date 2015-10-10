f5f93472c845fa5c55da20a8d9494c02f8c377a8
cristcost <cristiano.costantini@gmail.com>
2015-10-10 10:37:05 +0200

    ready for developing java

---
+++ java/dada-java-exercises/src/test/java/net/cristcost/data/dailyreport/DailyReportAppTest.java
@@ -0,0 +1,16 @@
+package net.cristcost.data.dailyreport;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DailyReportAppTest {
+
+  @Test
+  @Ignore
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
---
+++ java/dada-java-exercises/src/test/java/net/cristcost/data/multiply/MultiplyAppTest.java
@@ -0,0 +1,17 @@
+package net.cristcost.data.multiply;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class MultiplyAppTest {
+
+  @Test
+  @Ignore
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
