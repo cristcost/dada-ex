d57874af75219d13140ac6c7d296686123f0e521
cristcost <cristiano.costantini@gmail.com>
2015-10-11 01:16:48 +0200

    playing with cpp

--- cpp/src/multiply.cpp
+++ cpp/src/multiply.cpp
@@ -13,8 +13,26 @@
 
 using namespace std;
 
-int main(int argc, const char * argv[]) {
+bool validateString(const char* string) {
+    return false;
+}
 
+int main(int argc, const char * argv[]) {
+    
+    if(argc < 3) {
+        cout << "Usage:   multiply operand1 operand2" << endl;
+        cout << "Example: multiply 123 321" << endl;
+        exit(0);
+    }
+    if (!validateString(argv[1])) {
+        cout << "Invalid operand 1" << endl;
+        exit(0);
+    }
+    if (!validateString(argv[2])) {
+        cout << "Invalid operand 2" << endl;
+        exit(0);
+    }
+    
     cout << "Sandbox project for Multiply exercise" << endl;
     
     return 0;
